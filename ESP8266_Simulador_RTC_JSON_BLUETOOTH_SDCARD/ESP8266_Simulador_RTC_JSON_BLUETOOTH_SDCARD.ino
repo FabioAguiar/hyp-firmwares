7b1f997c583cbe3d9cdfe31701d7a8cb6360e2e5
Fabio Aguiar <fabioaguiar.ti@gmail.com>
2020-03-15 19:00:14 -0300

    update

--- ESP8266_Simulador_RTC_JSON_BLUETOOTH_SDCARD/ESP8266_Simulador_RTC_JSON_BLUETOOTH_SDCARD.ino
+++ ESP8266_Simulador_RTC_JSON_BLUETOOTH_SDCARD/ESP8266_Simulador_RTC_JSON_BLUETOOTH_SDCARD.ino
@@ -4,30 +4,48 @@
 #include <RTClib.h>
 #include <StringSplitter.h>
 #include <ArduinoJson.h>
+#include <SPI.h>
+#include <SD.h>
+
 
 #define DEBUG
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 RTC_DS1307 RTC;
+File myFile;
+Sd2Card card;
+SdVolume volume;
+SdFile root;
 
 int tempo = 2000;
 char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+int Year, Month, Day, Hour, Minute, Second;
 char buff [512];
-bool btPermission = true;
+String inputSerialString;
+bool serialPermission = true;
 
-StringSplitter *splitter;
+StringSplitter *split;
 
 struct wifi{
   char* ssid;
   char* password;
 };
 
-struct broker_mqtt{
-  char* mqttServer;
-  char* mqttUser;
-  char* mqttPassword;
-  uint16_t mqttPort;
+struct server_broker{
+  char* serverName;
+  char* serverUser;
+  char* serverPassword;
+  uint16_t serverPort;  
+};
+
+struct date_time{
+  int year;
+  int month;
+  int day;
+  int hour;
+  int minute;
+  int second;
 };
 
 struct global_helper{
@@ -65,28 +83,84 @@ struct split_topic{
 };
 
 struct wifi wifi_information;
-struct broker_mqtt server;
+struct server_broker broker;
 struct global_helper helper;
 struct mqtt_publish pub;
 struct mqtt_subscriber subs;
 struct split_topic st;
+struct date_time dt;
 
-void setInitWifi(){
-  //wifi_information = (wifi) {"DEN_BIOMASSA", "BIO_DEN_2014+"};  
-  wifi_information = (wifi) {"SUPERNET_FABYUU", "luci6666"};
-  //wifi_information = (wifi) {"Ap-F12", "08774576"};
+String ler_linha(int num_linha, char* arquivo) {
+  String linha = "";
+  myFile = SD.open(arquivo);
+  if (myFile) {
+    for (int i = 0; i < num_linha; i++) {
+      while (myFile.read() != '\n') {
+        ;
+      }
+    }
+    while (myFile.peek() != '\n') {
+      linha += char(myFile.read());
+    }
+  }
+  myFile.close();
+  split = new StringSplitter(linha, '|', 2);
+  linha = split->getItemAtIndex(1);
+  return linha;
 }
 
-void setWifi(char* ssid, char* password){
-  wifi_information = (wifi) {ssid, password};
+void setWifi(String ssid, String pass){
+
+  if (wifi_information.ssid != 0) {
+    delete [] wifi_information.ssid;
+  }
+  if (wifi_information.password != 0) {
+    delete [] wifi_information.password;
+  }
+
+  wifi_information.ssid = new char[ssid.length()];
+  ssid.toCharArray(wifi_information.ssid,ssid.length());
+
+  wifi_information.password = new char[pass.length()];
+  pass.toCharArray(wifi_information.password,pass.length());  
+}
+
+void setInitWifi(){
+  String ssid = ler_linha(0, "wifi.txt");
+  String pass = ler_linha(1, "wifi.txt");
+  setWifi(ssid, pass);
+}
+
+void setServerBroker(String serverName, String serverUser, String serverPassword, uint16_t serverPort){
+    if (broker.serverName != 0) {
+      delete [] broker.serverName;
+    }
+    if (broker.serverUser != 0) {
+      delete [] broker.serverUser;
+    }
+    if (broker.serverPassword != 0) {
+      delete [] broker.serverPassword;
+    }
+
+    broker.serverName = new char[serverName.length()];
+    serverName.toCharArray(broker.serverName,serverName.length());    
+
+    broker.serverUser = new char[serverUser.length()];
+    serverUser.toCharArray(broker.serverUser,serverUser.length());
+
+    broker.serverPassword = new char[serverPassword.length()];
+    serverPassword.toCharArray(broker.serverPassword,serverPassword.length());      
+
+    broker.serverPort = serverPort;
 }
 
 void setInitServerBroker(){
-  server = (broker_mqtt) {"m14.cloudmqtt.com", "cxhovvsp", "v-nT9GcH_-Cr", 14167};
-}
+  String serverName = ler_linha(0, "broker.txt");
+  String serverUser = ler_linha(1, "broker.txt");
+  String serverPassword = ler_linha(2, "broker.txt");
+  String serverPort = ler_linha(3, "broker.txt");
 
-void setServerBroker(char* mqttServer, char* mqttUser, char* mqttPassword, uint16_t mqttPort){
-  server = (broker_mqtt) {mqttServer, mqttUser, mqttPassword, mqttPort};
+  setServerBroker(serverName, serverUser, serverPassword, strtoul(serverPort.c_str(), NULL, 0));
 }
 
 void setInitGlobalHelper(){
@@ -188,41 +262,39 @@ void initWifi(){
   Serial.println("Conectado na rede WiFi");
   #endif
 
-  client.setServer(server.mqttServer, server.mqttPort);
+  client.setServer(broker.serverName, broker.serverPort);
   client.setCallback(callback);
  
   while (!client.connected()) {
     #ifdef DEBUG
     Serial.println("Conectando ao Broker MQTT...");
     #endif
- 
-    if (client.connect("ESP8266Client", server.mqttUser, server.mqttPassword )) {
+    
+    if (client.connect("ESP8266Client", broker.serverUser, broker.serverPassword )) {
       #ifdef DEBUG
       Serial.println("Conectado");  
       #endif
- 
+      
     } else {
       #ifdef DEBUG 
       Serial.print("falha estado  ");
       Serial.print(client.state());
       #endif
       delay(2000);
- 
     }
   }  
 }
-
 
 void initWire(){
     Wire.begin();  
 }
 
 void initRTC(){
-    RTC.begin();
+  RTC.begin();
   if (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    //RTC.adjust(DateTime(2020, 1, 7, 14, 46, 0));
-  }  
+    //Serial.println("RTC is NOT running!");
+    RTC.adjust(DateTime(2020, 1, 11, 11, 16, 0));
+  }
 }
 
 void reconect() {
@@ -232,8 +304,8 @@ void reconect() {
     Serial.print("Tentando conectar ao servidor MQTT");
     #endif
      
-    bool conectado = strlen(server.mqttUser) > 0 ?
-                     client.connect("ESP8266Client", server.mqttUser, server.mqttPassword) :
+    bool conectado = strlen(broker.serverUser) > 0 ?
+                     client.connect("ESP8266Client", broker.serverUser, broker.serverPassword) :
                      client.connect("ESP8266Client");
  
     if(conectado) {
@@ -257,7 +329,94 @@ void reconect() {
   }
 }
 
+void initSDCard(){
+
+  Serial.print("\nInicializando SD card...");
+
+  if ( !SD.begin() ){
+    Serial.println("falha na inicialização do cartão!");
+  }
+  Serial.println("Inicialização do cartão concluída.");
+
+  if (SD.exists("log.txt")) {
+    Serial.println("O arquivo log.txt existe no cartão.");
+  } else {
+    Serial.println("Criando arquivo log.txt...");
+    myFile = SD.open("log.txt", FILE_WRITE);
+    myFile.close();
+  }
+
+  if (SD.exists("wifi.txt")) {
+    Serial.println("O arquivo wifi.txt existe no cartão.");
+  } else {
+    Serial.println("Criando arquivo wifi.txt...");
+    myFile = SD.open("wifi.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.println("ssid|");
+      myFile.println("pass|");
+      myFile.close();
+      Serial.println("Arquivo wifi.txt criado.");
+    } else {
+      Serial.println("Erro ao abrir wifi.txt");
+    }
+    myFile.close();
+  }
+
+  if (SD.exists("broker.txt")) {
+    Serial.println("O arquivo broker.txt existe no cartão.");
+  } else {
+    Serial.println("Criando arquivo broker.txt...");
+    myFile = SD.open("broker.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.println("serverName|");
+      myFile.println("serverUser|");
+      myFile.println("serverPassword|");
+      myFile.println("serverPort|");
+      myFile.close();
+      Serial.println("Arquivo broker.txt criado.");
+    } else {
+      Serial.println("Erro ao abrir broker.txt");
+    }
+    myFile.close();    
+  }
+
+  if (SD.exists("topicos.txt")) {
+    Serial.println("O arquivo topicos.txt existe no cartão.");
+  } else {
+    Serial.println("Criando arquivo topicos.txt...");
+    myFile = SD.open("topicos.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.println("sensor|");
+      myFile.println("atuador|");
+      myFile.close();
+      Serial.println("Arquivo topicos.txt criado.");
+    } else {
+      Serial.println("Erro ao abrir topicos.txt");
+    }
+    myFile.close();
+  }
+  
+
+  if (SD.exists("config.txt")) {
+    Serial.println("O arquivo config.txt existe no cartão.");
+  } else {
+    Serial.println("Criando arquivo config.txt...");
+    myFile = SD.open("config.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.println("teste|");
+      myFile.close();
+      Serial.println("Arquivo config.txt criado.");
+    } else {
+      Serial.println("Erro ao abrir config.txt");
+    }
+    myFile.close();
+  }
+
+}
+
 void initConfig(){
+  initSDCard();
+  cardInformation();  
   setInitGlobalHelper();
   setInitWifi();
   setInitServerBroker();
@@ -269,13 +428,42 @@ void initConfig(){
   client.subscribe(subs.actuator3);
   client.subscribe(subs.actuator4);
   initWire();
-  initRTC();
+  initRTC();  
 }
 
 void setup() {
   Serial.begin(9600);
   initConfig();
 }
+
+void setDateTime(String dateTime){
+  sscanf(dateTime.c_str(), "%d-%d-%d %d:%d:%d", &Year, &Month, &Day, &Hour, &Minute, &Second);
+}
+
+void setDateTime(int data,int pos){
+
+  switch(pos){
+    case 0:
+      dt.year = data;  
+      break;
+    case 1:
+      dt.month = data;    
+      break;
+    case 2:
+      dt.day = data;
+      break;
+    case 3:
+      dt.hour = data;
+      break;
+    case 4:
+      dt.minute = data;
+      break;  
+    case 5:
+      dt.second = data;    
+      break;
+  }
+}
+
 
 String dateTimeNow(){
     DateTime now = RTC.now(); 
@@ -314,7 +502,7 @@ DynamicJsonDocument docJSON(StringSplitter* topicSensor){
 void sendHumidSoil(DynamicJsonDocument doc, char* topic){
   float humidSoil = random(0, 1024);
   doc["payload"] = humidSoil;
-  serialPrintJson(doc, btPermission);
+  serialPrintJson(doc, serialPermission);
   serializeJson(doc, buff);
   client.publish(topic, buff, true);
   //client.publish(topic, String(humidSoil).c_str(), true);
@@ -323,7 +511,7 @@ void sendHumidSoil(DynamicJsonDocument doc, char* topic){
 void sendHumid(DynamicJsonDocument doc, char* topic){
   float humid = random(15, 20);
   doc["payload"] = humid;
-  serialPrintJson(doc, btPermission);
+  serialPrintJson(doc, serialPermission);
   serializeJson(doc, buff);
   client.publish(topic, buff, true);  
   //client.publish(pub.sensor2, String(humid).c_str(), true);
@@ -332,7 +520,7 @@ void sendHumid(DynamicJsonDocument doc, char* topic){
 void sendTemp(DynamicJsonDocument doc, char* topic){
   float temp = random(25, 31);
   doc["payload"] = temp;
-  serialPrintJson(doc, btPermission);
+  serialPrintJson(doc, serialPermission);
   serializeJson(doc, buff);
   client.publish(topic, buff, true);  
   //client.publish(pub.sensor3, String(temp).c_str(), true);
@@ -341,7 +529,7 @@ void sendTemp(DynamicJsonDocument doc, char* topic){
 void sendLumen(DynamicJsonDocument doc, char* topic){
   float lumen = random(0, 65000);
   doc["payload"] = lumen;
-  serialPrintJson(doc, btPermission);
+  serialPrintJson(doc, serialPermission);
   serializeJson(doc, buff);
   client.publish(topic, buff, true); 
   //client.publish(pub.sensor4, String(lumen).c_str(), true);
@@ -350,7 +538,7 @@ void sendLumen(DynamicJsonDocument doc, char* topic){
 void sendpH(DynamicJsonDocument doc, char* topic){
   float pH = random(5, 7);
   doc["payload"] = pH;  
-  serialPrintJson(doc, btPermission);
+  serialPrintJson(doc, serialPermission);
   serializeJson(doc, buff);
   client.publish(topic, buff, true); 
   //client.publish(pub.sensor5, String(pH).c_str(), true);
@@ -384,21 +572,93 @@ void sendMQTT(){
   sendSensor5(st.splitTopicSensor5);
 }
 
-void btRead(){
-  if (Serial.available()){
-    char data_received; 
-    data_received = Serial.read();
-    if (data_received == '1'){
-      btPermission = true;
-    }else if (data_received == '0'){
-      btPermission = false;
-    }
+void cardInformation(){
+
+  if (!card.init()) {
+    Serial.println("Falha na inicialização.");
+    Serial.println("* O cartão foi inserido?");
+    return;
+  } else {
+    Serial.println("As conexões estão corretas e o cartão esta presente.");
+  }
+  
+  // Immprime o tipo do cartão
+  Serial.print("\nTipo do cartão: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Desconhecido");
+  }
+
+  if (!volume.init(card)) {
+    Serial.println("Não foi encontrado nenhum cartão com partição FAT16/FAT32.\n Confira se o cartão foi formatado adequadamente.");
+    return;
   }  
+
+  Serial.println("\nArquivos encontrados no cartão (Nome, data e tamanho em bytes): ");
+  root.openRoot(volume);
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+}
+
+void menuSerial(){
+  Serial.println("1 - Exibe os dados na porta serial");  
+  Serial.println("2 - Para de exibir os dados na porta serial");
+  Serial.println("3 - Exibe informações do SD Card");
+  Serial.println("4 - Exibe Data e Hora atual");
+  Serial.println("5 - Seta Data e Hora");
+
+  Serial.println("9 - Exibe menu");  
 }
 
 
+String serialEvent() {
+  char inChar;
+  
+  while (Serial.available()) {
+    inChar = (char)Serial.read();
+  }
+
+  return (String)inChar;
+}
+
+void refreshDateTime(){
+  
+}
+
+void switchSerialEvents(){
+  String event = serialEvent();
+  //Serial.println(event);
+  
+  if(event == "1"){
+  //Exibe os dados na porta serial
+    serialPermission = true;
+  }else if(event == "2"){
+  //Para de exibir os dados na porta serial
+    serialPermission = false;
+  }else if(event == "3"){
+  //Exibe informações do SD Card      
+    cardInformation();
+    serialPermission = false;
+    }else if(event == "5"){
+  //Exibe Data e Hora atual do dispositivo
+  Serial.println(dateTimeNow());
+  serialPermission = false;
+  }else if(event == "9"){
+    menuSerial();
+    serialPermission = false;
+  }
+}
+  
 void loop() {
-  btRead();
+  switchSerialEvents();
   
   if (!client.connected()) {
     reconect();
@@ -411,3 +671,20 @@ void loop() {
 
   client.loop();  
 }
+
+byte decToBcd(byte val){
+ // Convert normal decimal numbers to binary coded decimal
+ return ( (val/10*16) + (val%10) );
+}
+
+//void freeSpaceDT(){
+//  if (dt.year != 0 || dt.month != 0|| dt.day != 0 || dt.hour!= 0 || dt.minute != 0 || dt.second != 0) {
+//    delete [] dt.year;
+//    delete [] dt.month;
+//    delete [] dt.day;
+//    delete [] dt.hour;
+//    delete [] dt.minute;
+//    delete [] dt.second;
+//  }
+//}
+
